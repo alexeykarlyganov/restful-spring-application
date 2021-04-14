8519888dd7a2edecccea93db1e979a98d8d3e7ab
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2021-04-14 15:36:21 +0300

    added service for send and rececive messages within kafka

---
+++ src/main/java/org/alexeykarlyganov/rest/services/profile/EntryService.java
@@ -0,0 +1,11 @@
+package org.alexeykarlyganov.rest.services.profile;
+
+import org.alexeykarlyganov.rest.models.Entry;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface EntryService {
+    void send(Entry entry);
+    void receive(@Payload Entry user, @Headers MessageHeaders headers);
+}
---
+++ src/main/java/org/alexeykarlyganov/rest/services/profile/EntryServiceImpl.java
@@ -0,0 +1,60 @@
+package org.alexeykarlyganov.rest.services.profile;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.alexeykarlyganov.rest.models.Entry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EntryServiceImpl implements EntryService {
+
+    private final KafkaTemplate<String, Entry> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public EntryServiceImpl(KafkaTemplate<String, Entry> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void send(Entry entry) {
+        log.info("<= Sending new {}", this.jsonToString(entry));
+
+        Message<Entry> message = MessageBuilder
+                    .withPayload(entry)
+                    .setHeader(KafkaHeaders.TOPIC, "server.users")
+                    .build();
+        kafkaTemplate.send(message);
+    }
+
+    @Override
+    @KafkaListener(topics = {"server.users"}, groupId = "server.broadcast")
+    public void receive(@Payload Entry entry, @Headers MessageHeaders headers) {
+        log.info("=> consumed {}", this.jsonToString(entry));
+
+        headers.keySet().forEach(key -> {
+            log.info("{} --- {}", key, headers.get(key));
+        });
+    }
+
+    private String jsonToString(Entry entry) {
+        try {
+            return objectMapper.writeValueAsString(entry);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Could not write JSON info: " + ex.toString());
+        }
+    }
+}
