a7426e284c65d7d31ab9306258ec240cb3b3e009
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2021-04-16 10:03:40 +0300

    remove annotation @EnableKafka

--- src/main/java/org/alexeykarlyganov/rest/config/KafkaConsumerConfiguration.java
+++ src/main/java/org/alexeykarlyganov/rest/config/KafkaConsumerConfiguration.java
@@ -6,7 +6,6 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
@@ -18,7 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableKafka
 public class KafkaConsumerConfiguration {
 
     @Value("${kafka.bootstrap-servers}")
--- src/main/java/org/alexeykarlyganov/rest/config/KafkaProducerConfiguration.java
+++ src/main/java/org/alexeykarlyganov/rest/config/KafkaProducerConfiguration.java
@@ -6,7 +6,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -17,20 +16,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableKafka
 public class KafkaProducerConfiguration {
     @Value("${kafka.bootstrap-servers}")
     private String kafkaServer;
-
-    //@Value("${kafka.producer.id}")
-   // private String kafkaProducerId;
 
     @Bean
     public Map<String, Object> producerConfig() {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaServer);
-        //props.put(ProducerConfig.CLIENT_ID_CONFIG, this.kafkaProducerId);
 
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
