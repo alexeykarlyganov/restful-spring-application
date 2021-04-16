7b9d69167cce3908ed8cd8851652b4329990e91b
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2021-04-16 17:36:56 +0300

    refactor PingController

--- src/main/java/org/alexeykarlyganov/rest/controllers/system/PingController.java
+++ src/main/java/org/alexeykarlyganov/rest/controllers/system/PingController.java
@@ -5,20 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.alexeykarlyganov.rest.services.BuildInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
 
 @RestController
+@RequestMapping(value = "/system")
 public class PingController {
-    @Autowired
     private BuildInfoService buildInfoService;
-
-    @Autowired
     private ObjectMapper objectMapper;
 
-    @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Autowired
+    public PingController(BuildInfoService buildInfoService, ObjectMapper objectMapper) {
+        this.buildInfoService = buildInfoService;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(value = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getProperties() throws JsonProcessingException
     {
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(buildInfoService.getBuildInfo());
