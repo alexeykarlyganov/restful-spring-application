633402c6371dbf0988cc8be19377c5f3d4a7faea
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2021-04-16 17:41:14 +0300

    added comments to EntryService's methods

--- src/main/java/org/alexeykarlyganov/rest/services/entry/EntryService.java
+++ src/main/java/org/alexeykarlyganov/rest/services/entry/EntryService.java
@@ -6,6 +6,16 @@ import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 
 public interface EntryService {
+    /**
+     * Send a Entry object to the kafka
+     * @param entry - A entry object
+     */
     void send(Entry entry);
-    void receive(@Payload Entry user, @Headers MessageHeaders headers);
+
+    /**
+     * Receive a single Entry object
+     * @param entry - A entry object
+     * @param headers - A message headers
+     */
+    void receive(@Payload Entry entry, @Headers MessageHeaders headers);
 }
