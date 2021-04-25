bd3e1ce6d628102d1cd58c3251a84e836652dd32
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2021-04-25 16:08:39 +0300

    added @EnableKafka

--- src/main/java/org/alexeykarlyganov/rest/config/KafkaConsumerConfiguration.java
+++ src/main/java/org/alexeykarlyganov/rest/config/KafkaConsumerConfiguration.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
@@ -15,6 +16,7 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfiguration {
 
--- src/main/java/org/alexeykarlyganov/rest/config/KafkaProducerConfiguration.java
+++ src/main/java/org/alexeykarlyganov/rest/config/KafkaProducerConfiguration.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,6 +16,7 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaProducerConfiguration {
     @Value("${kafka.bootstrap-servers}")
